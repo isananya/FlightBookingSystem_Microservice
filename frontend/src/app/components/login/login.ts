1f47325473b8bc661edae31d8a06f7b8ddd6a22f
isananya <nayakananya2004@gmail.com>
2025-12-15 00:12:45 +0530

    feat: add basic login page

--- frontend/src/app/components/login/login.ts
+++ frontend/src/app/components/login/login.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {
 
+export class Login {
+  email = '';
+  password = '';
+  message = '';
+
+  constructor(private authService: AuthService) {}
+
+  login() {
+    this.authService.login({
+      email: this.email,
+      password: this.password
+    }).subscribe({
+      next: res => this.message = res,
+      error: err => this.message = err.error
+    });
+  }
 }
--- frontend/src/app/services/auth.ts
+++ frontend/src/app/services/auth.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Auth {
-  
+export class AuthService {
+  private baseUrl = environment.apiGatewayUrl + '/auth';
+
+  constructor(private http: HttpClient) {}
+
+  login(data: any) {
+    return this.http.post(`${this.baseUrl}/login`, data, {
+      responseType: 'text'
+    });
+  }
 }
