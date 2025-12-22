cd5c40c4fc48b01cb968b138e0264dbbd31a4030
isananya <nayakananya2004@gmail.com>
2025-12-22 21:05:25 +0530

    chore: set up manage inventory component

---
+++ frontend/src/app/pages/manage-inventory/manage-inventory.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-inventory',
+  imports: [],
+  templateUrl: './manage-inventory.html',
+  styleUrl: './manage-inventory.css',
+})
+export class ManageInventory {
+
+}
